ea31181329d4098a67c756d7a060901a03313be5
Anointing-Olabode <anointingolabode@gmail.com>
2024-11-01 03:36:35 -0700

    Add files via upload

---
+++ index.html.js.2/script.js
@@ -0,0 +1,7 @@
+function changeBackgroundColor() {
+    let color= ["aqua"]
+    document.getElementById("body").style.backgroundColor= color[0]
+}
+function changeGreeting() {
+document.getElementById("text").innerHTML = `"Glad you’re here! Enjoy exploring!"`
+}
